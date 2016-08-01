89f41ae36d3e232598f9586fbe370f5eeba38627
chenzhou <chenzhou@thoughtworks.com>
2016-08-01 21:45:58 +0800

    [chenzhou]should display books that are return when calling listbooks

--- test/com/twu/biblioteca/BibliotecaServiceTest.java
+++ test/com/twu/biblioteca/BibliotecaServiceTest.java
@@ -89,8 +89,32 @@ public class BibliotecaServiceTest {
     }
 
     @Test
-    public void should_return_false_when_call_returnBook_if_the_book_has_not_been_checked_out_and_the_book_dose_not_exist(){
+    public void should_return_false_when_call_returnBook_if_the_book_has_not_been_checked_out_or_the_book_dose_not_exist(){
         assertFalse(bibliotecaService.ReturnBooks("invaild name"));
         assertFalse(bibliotecaService.ReturnBooks("english"));
+    }
+
+    @Test
+    public void Should_display_books_that_are_return_when_calling_listBooks(){
+        bibliotecaService.CheckoutBooks("math");
+        List<Book> books = bibliotecaService.ListBooks();
+        assertEquals("chinese",books.get(0).getName());
+        assertEquals("english",books.get(1).getName());
+        assertEquals("huawu",books.get(0).getAuthor());
+        assertEquals("danhu",books.get(1).getAuthor());
+        assertEquals("2011-09-14",books.get(0).getPublishedYear());
+        assertEquals("2015-05-10",books.get(1).getPublishedYear());
+
+        bibliotecaService.ReturnBooks("math");
+        List<Book> booksReturn = bibliotecaService.ListBooks();
+        assertEquals("math",booksReturn.get(0).getName());
+        assertEquals("chinese",booksReturn.get(1).getName());
+        assertEquals("english",booksReturn.get(2).getName());
+        assertEquals("yangliu",booksReturn.get(0).getAuthor());
+        assertEquals("huawu",booksReturn.get(1).getAuthor());
+        assertEquals("danhu",booksReturn.get(2).getAuthor());
+        assertEquals("2013-10-10",booksReturn.get(0).getPublishedYear());
+        assertEquals("2011-09-14",booksReturn.get(1).getPublishedYear());
+        assertEquals("2015-05-10",booksReturn.get(2).getPublishedYear());
     }
 }
